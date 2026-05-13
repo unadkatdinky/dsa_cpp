50bd393b7ac4fb6736146d9f03de7c2c90b5e951
Dinky Unadkat <161971995+unadkatdinky@users.noreply.github.com>
2026-05-13 23:22:49 +0530

    Implement pattern printing functions in C++
    Added functions to print patterns based on input size.

--- Patterns/solving_patterns.cpp
+++ Patterns/solving_patterns.cpp
@@ -14,6 +14,29 @@ void print2(int n) {
         cout << endl;
     }
 }
+
+void print3(int n) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print4(int n) {
+    int count = 1;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << count << " "; 
+           
+            
+        }
+        cout << endl;
+        count++;
+    }
+}
+
 int main() {
     // qn 1
     //  ****
@@ -21,7 +44,7 @@ int main() {
     //  ****
     //  ****
     //in each line we are printing 4 stars (4 rows , 4 stars )
-    
+    cout << "Question 1" << endl;
     for(int i = 0; i< 4; i++) {
         for(int j = 0; j < 4; j++) {
            cout << "*";
@@ -33,7 +56,11 @@ int main() {
     cout << "Question 2:" << endl;
     int i = 7;
     print2(i);
-    
-    
+    cout << endl; 
+    cout << "Question 2:" << endl;
+    print3(i);
+    cout << endl; 
+    cout << "Question 3:" << endl;
+    print4(i);
     
 }
